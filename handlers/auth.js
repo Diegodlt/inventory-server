f8d08538df9154dd5de4f2263895ba4e04b8e1e7
Diego <diegodltl@gmail.com>
2019-07-27 12:00:46 -0400

    Implement user model and login handler

---
+++ handlers/auth.js
@@ -0,0 +1,35 @@
+const db = require('../models');
+const jwt = require('jsonwebtoken');
+
+exports.signin = async function(req, res, next){
+    try{
+        let user = await db.User.findOne({
+            email: req.body.email
+        });
+        let { id, username, email } = user;
+        let isMatch = await user.comparePassword(req.body.password);
+        if(isMatch){
+            let token = jwt.sign(
+                {
+                    id,
+                    username,
+                    email
+                },
+                process.env.SECRET_KEY
+            );
+            return res.status(200).json({
+                id,
+                username,
+                email,
+                token
+            });
+        }else{
+            return next({
+                status: 400,
+                message: "Invalid Email/Password"
+            })
+        }
+    }catch(err){
+        return next({status: 400, message: "Invalid Email/Password."})
+    }
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const errorHandler = require('./handlers/error');
+const authRoutes = require('./routes/auth')
+
+const PORT = 3200;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+    res.send("Hello World!");
+});
+
+app.use('/api/auth', authRoutes)
+
+app.use((req, res, next)=>{
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use(errorHandler);
+
+
+app.listen(PORT,()=>{
+    console.log("Server is running...");
+})
